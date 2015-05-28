324b35254233544f6b9577c9069a0aa7ef5d4781
Stephen <Sack11man@aol.com>
2015-05-27 22:44:19 -0500

    Placed icon in the icon tray, removed background, and evenly spaced them.

--- www/js/iconLayer.js
+++ www/js/iconLayer.js
@@ -1,3 +1,7 @@
 /**
- * Created by marisabel4171 on 5/26/2015.
+ * Created by Stephen Kuehl on 5/26/2015.
  */
+    function initialize(){
+
+    var profilebutton = document.images["profile"];
+}
--- www/js/map.js
+++ www/js/map.js
@@ -6,7 +6,8 @@ var map;
 
 function initialize() {
     var mapOptions = {
-        zoom: 15
+        zoom: 15,
+        disableDefaultUI: true
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
